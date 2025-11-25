eda9b44f958a50ed185a683213dd1c4bf54e377f
nuonuo-888 <2720161028@qq.com>
2025-11-25 19:02:39 +0800

    chore: upgrade Astro to 5.16.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import edgeongeAdapter from "@edgeone/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: edgeongeAdapter(),
 });
